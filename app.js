7ebfba456b9073283d99b26373cb12e2086140ac
nevmerzhitskaya <lamara@tut.by>
2023-04-18 18:58:06 +0200

    refactor(basic-3.1): update class and id naming according to BEM

--- app.js
+++ app.js
@@ -8,42 +8,53 @@
 
 // Event handling, user interaction is what starts the code execution.
 
-var taskInput = document.getElementById("new-task");//Add a new task.
-var addButton = document.getElementsByTagName("button")[0];//first button
-var incompleteTaskHolder = document.getElementById("incompleteTasks");//ul of #incompleteTasks
-var completedTasksHolder = document.getElementById("completed-tasks");//completed-tasks
+var taskInput = document.querySelector(".js-add-task");//Add a new task.
+var addButton = document.querySelector(".js-add-btn");//first button
+var incompleteTaskHolder = document.querySelector('.js-incompleted-tasks');//ul of #incompleteTasks
+var completedTasksHolder = document.querySelector('.js-completed-tasks');//completed-tasks
 
 
 //New task list item
 var createNewTaskElement = function (taskString) {
 
   var listItem = document.createElement("li");
+      listItem.classList.add('task-list__item');
 
   //input (checkbox)
-  var checkBox = document.createElement("input");//checkbx
+  var checkBox = document.createElement("input");
+      checkBox.classList.add('form__checkbox');//checkbx
   //label
   var label = document.createElement("label");//label
   //input (text)
-  var editInput = document.createElement("input");//text
+  var editInput = document.createElement("input");
+      editInput.classList.add('flex-grow-1');
+      editInput.classList.add('form__input');
+      editInput.classList.add('js-input');
   //button.edit
-  var editButton = document.createElement("button");//edit button
+  var editButton = document.createElement("button");
+      editButton.classList.add('form__btn');
+      editButton.classList.add('js-edit-btn');//edit button
 
   //button.delete
-  var deleteButton = document.createElement("button");//delete button
-  var deleteButtonImg = document.createElement("img");//delete button image
+  var deleteButton = document.createElement("button");
+      deleteButton.classList.add('form__btn');
+      deleteButton.classList.add('form__btn_delete');
+      deleteButton.classList.add('js-delete-btn');//delete button
+  var deleteButtonImg = document.createElement("img");
+      deleteButtonImg.classList.add('form__icon_delete');//delete button image
 
   label.innerText = taskString;
-  label.className = "task";
+  label.className = "flex-grow-1";
 
   //Each elements, needs appending
   checkBox.type = "checkbox";
   editInput.type = "text";
-  editInput.className = "task";
+  editInput.className = "flex-grow-1 form__input js-input";
 
   editButton.innerText = "Edit"; //innerText encodes special characters, HTML does not.
-  editButton.className = "edit";
+  editButton.className = "form__btn js-edit-btn";
 
-  deleteButton.className = "delete";
+  deleteButton.className = "form__btn form__btn_delete js-delete-btn";
   deleteButtonImg.src = "./remove.svg";
   deleteButton.appendChild(deleteButtonImg);
 
@@ -82,14 +93,14 @@ var editTask = function () {
 
   var listItem = this.parentNode;
 
-  var editInput = listItem.querySelector("input[type=text]");
-  var label = listItem.querySelector("label");
-  var editBtn = listItem.querySelector(".edit");
-  var containsClass = listItem.classList.contains("editMode");
-  //If class of the parent is .editmode
+  var editInput = listItem.querySelector(".js-input");
+  var label = listItem.querySelector(".js-label");
+  var editBtn = listItem.querySelector(".js-edit-btn");
+  var containsClass = listItem.classList.contains("form__edit-mode");
+  //If class of the parent is .form__edit-mode
   if (containsClass) {
 
-    //switch to .editmode
+    //switch to .form__edit-mode
     //label becomes the inputs value.
     label.innerText = editInput.value;
     editBtn.innerText = "Edit";
@@ -98,8 +109,8 @@ var editTask = function () {
     editBtn.innerText = "Save";
   }
 
-  //toggle .editmode on the parent.
-  listItem.classList.toggle("editMode");
+  //toggle .form__edit-mode on the parent.
+  listItem.classList.toggle("form__edit-mode");
 };
 
 
@@ -155,9 +166,9 @@ addButton.addEventListener("click", ajaxRequest);
 var bindTaskEvents = function (taskListItem, checkBoxEventHandler) {
   console.log("bind list item events");
   //select ListItems children
-  var checkBox = taskListItem.querySelector("input[type=checkbox]");
-  var editButton = taskListItem.querySelector("button.edit");
-  var deleteButton = taskListItem.querySelector("button.delete");
+  var checkBox = taskListItem.querySelector(".form__checkbox");
+  var editButton = taskListItem.querySelector(".js-edit-btn");
+  var deleteButton = taskListItem.querySelector(".js-delete-btn");
 
 
   //Bind editTask to edit button.
